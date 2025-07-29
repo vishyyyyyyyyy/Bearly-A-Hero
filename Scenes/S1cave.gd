f1c0feb71eacf1600f86485306670ad2424ac785
vishyyyyyyyyy <217375979+vishyyyyyyyyy@users.noreply.github.com>
2025-07-29 16:52:43 -0500

    S1 Added Collision walls to border and bear

--- Scenes/S1cave.gd
+++ Scenes/S1cave.gd
@@ -1,11 +1,18 @@
 extends Node2D
+
 func _ready():
-	pass
+	$Timer.start()
+	$CharacterBody2D.visible = false
 
 
 
 func show_text_and_tile():
 
-	$LightmapGI.visible = true
+	$"S1-text1".visible = true
 
 	
+
+
+func _on_timer_timeout():
+	$"S1-text1".visible = false
+	$CharacterBody2D.visible = true
---
+++ Scenes/main_scene.gd
@@ -0,0 +1,4 @@
+extends Node2D
+
+func _ready():
+	load("res://Scenes/Cave.tscn")
