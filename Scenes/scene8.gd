3e5bfcd66bfd8d37e4c0e86df9026bb26831bfdf
vishyyyyyyyyy <217375979+vishyyyyyyyyy@users.noreply.github.com>
2025-07-30 14:56:00 -0500

    update scenes

---
+++ Scenes/scene8.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+func _ready():
+	$Headphone.visible = false
+	$"S5-text1".visible = true
+	$CharacterBody2D.can_move = true
+
+func _input(event):
+	if Input.is_action_just_pressed("drop_scarf"):
+		drop_headphone()
+
+func drop_headphone(): 
+	$Headphone.visible = true  # or queue_free() if you want to delete it
+	print("Scarf dropped!")
+	$ExitTrigger.set_headphone_dropped()
+	$"S5-text1".visible = false
+	$"S5-text2".visible = true
+	
