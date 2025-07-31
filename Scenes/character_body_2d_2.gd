4ce61e6def680e0ffde317d666361f4f746a42ec
vishyyyyyyyyy <217375979+vishyyyyyyyyy@users.noreply.github.com>
2025-07-31 17:24:19 -0500

    working scene 14

---
+++ Scenes/character_body_2d_2.gd
@@ -0,0 +1,4 @@
+extends CharacterBody2D
+
+func _ready() -> void:
+	$AnimatedSprite2D.flip_h = true
---
+++ Scenes/scene14.gd
@@ -0,0 +1,15 @@
+extends Node2D
+func _ready():
+	$bearFox.can_move = true
+@onready var animated_sprite_2d: AnimatedSprite2D = $CharacterBody2D2/AnimatedSprite2D
+@onready var anim: AnimatedSprite2D = $bearFox/anim
+@onready var collision_shape_2d: CollisionShape2D = $Area2D/CollisionShape2D
+
+
+
+
+func _on_area_2d_body_entered(body: CharacterBody2D) -> void:
+	animated_sprite_2d.flip_h = false # Replace with function body.
+	animated_sprite_2d.play("attack")
+	$bearFox/anim.play("death")
+	collision_shape_2d.disabled = true
