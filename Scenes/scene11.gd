66622d631d1af862c2eda70d68f957d80f89cd2f
vishyyyyyyyyy <217375979+vishyyyyyyyyy@users.noreply.github.com>
2025-07-31 10:07:50 -0500

    scene11to12

---
+++ Scenes/scene11.gd
@@ -0,0 +1,71 @@
+extends Node2D
+
+@onready var timer: Timer = $Timer
+@onready var safe_timer: Timer = $safeTimer
+var bodyCounter = 0
+@onready var kill_timer: Timer = $kill/killTimer
+@onready var character_body_2d: CharacterBody2D = $CharacterBody2D
+
+
+func _ready() -> void:
+	$CharacterBody2D.can_move = true
+	var killColl = get_node("kill/killcoll")
+	killColl.disabled = true
+	var nextScene = get_node("Area2D2/CollisionShape2D")
+
+	
+	# Ensure they are one-shot so we can alternate
+	timer.one_shot = true
+	safe_timer.one_shot = true
+
+func _on_tree_1_body_entered(body: CharacterBody2D) -> void:
+	print("entered")
+	bodyCounter += 1
+	print(bodyCounter)
+
+func _on_tree_1_body_exited(body: CharacterBody2D) -> void:
+	bodyCounter -= 1 # Replace with function body.
+	print(bodyCounter)
+	
+func _on_remaining_body_entered(body: CharacterBody2D) -> void:
+	bodyCounter += 1 # Replace with function body.
+	print(bodyCounter)
+	
+func _on_remaining_body_exited(body: CharacterBody2D) -> void:
+	bodyCounter -= 1 # Replace with function body.
+	print(bodyCounter)
+	
+func _on_kill_body_entered(body: CharacterBody2D) -> void:
+	bodyCounter += 1 # Replace with function body.
+	print(bodyCounter)
+	if bodyCounter == 1:
+		Engine.time_scale = 0.5
+		$kill/killTimer.start()
+		
+		
+func _on_kill_timer_timeout() -> void:
+	Engine.time_scale = 1.0
+	get_tree().reload_current_scene() # Replace with function body.
+		
+	
+func _on_kill_body_exited(body: CharacterBody2D) -> void:
+	bodyCounter -= 1
+	print(bodyCounter)
+
+
+func _on_timer_timeout() -> void:
+	var birdSprite = get_node("bird/AnimatedSprite2D")
+	birdSprite.flip_h = false
+	var killColl = get_node("kill/killcoll")
+	killColl.disabled = false
+	
+	safe_timer.start()  # Start the next phase after timer ends
+
+func _on_safe_timer_timeout() -> void:
+	var birdSprite = get_node("bird/AnimatedSprite2D")
+	birdSprite.flip_h = true
+	var killColl = get_node("kill/killcoll")
+	killColl.disabled = true
+	
+	timer.start()  # Loop back to the first phase
+	
--- Scenes/teddy_bear.gd
+++ Scenes/teddy_bear.gd
@@ -1,7 +1,11 @@
 extends Area2D
 
-
-
 func _on_body_entered(body: Node2D) -> void:
-	print("teddyyyy") # Replace with function body.
-	queue_free()
+	if body.name == "CharacterBody2D":
+		print("teddyyyyy picked up!")
+
+		var trigger = get_parent().get_node("SceneTrigger")
+		trigger.set_teddy_picked_up()
+
+		call_deferred("queue_free")  
+		# there some problem when i just do normal queue but it stop breaking when i do this soooo
