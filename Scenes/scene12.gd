88530da8e00c8323b389185eccaaacf6527abd85
vishyyyyyyyyy <217375979+vishyyyyyyyyy@users.noreply.github.com>
2025-07-31 17:01:14 -0500

    scene12

--- Scenes/scene12.gd
+++ Scenes/scene12.gd
@@ -8,11 +8,19 @@ func _ready():
 func _input(event):
 	if Input.is_action_just_pressed("drop_object"):
 		drop_teddy()
+		cut_scene()
+		
 
 func drop_teddy(): 
 	$TeddyBear.visible = true  # or queue_free() if you want to delete it
 	print("teddy dropped!")
 	$ExitTrigger.set_teddy_dropped()
 	$"S5-text1".visible = false
-	$"S5-text2".visible = true
+	#$"S5-text2".visible = true
 	
+func cut_scene(): 
+	$transition._transition()
+
+
+func _on_timer_timeout() -> void:
+	pass # Replace with function body.
