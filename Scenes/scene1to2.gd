c9dbaa72f4d0afff749cbbfe418f977b4ec7ab8b
vishyyyyyyyyy <217375979+vishyyyyyyyyy@users.noreply.github.com>
2025-07-31 21:55:11 -0500

    scene 1 update

--- Scenes/scene1to2.gd
+++ Scenes/scene1to2.gd
@@ -1,15 +1,11 @@
 extends Area2D
 
-var entered = false
+var scene_changed = false
 
 func _on_body_entered(body: Node2D) -> void:
-	entered = true # Replace with function body.
-
-
+	if not scene_changed and body.name == "CharacterBody2D":
+		scene_changed = true
+		get_tree().change_scene_to_file("res://Scenes/scene2.tscn")
 
 func _on_body_exited(body: Node2D) -> void:
-	entered = true # Replace with function body.
-
-func _process(delta):
-	if entered == true:
-		get_tree().change_scene_to_file("res://Scenes/scene2.tscn")
+	pass  # You can remove this if unused
