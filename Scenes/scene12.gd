90ccd682b2b6db4bf8207df2dbbb2a86fc08b7c0
vishyyyyyyyyy <217375979+vishyyyyyyyyy@users.noreply.github.com>
2025-08-01 00:53:05 -0500

    scene 12 update

--- Scenes/scene12.gd
+++ Scenes/scene12.gd
@@ -1,15 +1,19 @@
 extends Node2D
 
+var cutscene_played = false  # Flag to track cutscene state
+
 func _ready():
 	$TeddyBear.visible = false
 	$"S5-text1".visible = true
 	$CharacterBody2D.can_move = true
+	$"S12-text4".visible = false
 
 func _input(event):
 	if Input.is_action_just_pressed("drop_object"):
 		drop_teddy()
-		cut_scene()
-		
+		if not cutscene_played:
+			cut_scene()
+			cutscene_played = true  # Mark cutscene as played
 
 func drop_teddy(): 
 	$TeddyBear.visible = true  # or queue_free() if you want to delete it
@@ -17,10 +21,19 @@ func drop_teddy():
 	$ExitTrigger.set_teddy_dropped()
 	$"S5-text1".visible = false
 	#$"S5-text2".visible = true
-	
+
 func cut_scene(): 
 	$transition._transition()
+	await get_tree().create_timer(4.0).timeout
+	secondhalf()
 
+	
 
-func _on_timer_timeout() -> void:
-	pass # Replace with function body.
+func secondhalf():
+	$Pile.visible = true
+	$"S5-text1".visible = false
+	$Textplaceholder.visible = true
+	$Pfpplaceholder.visible = true
+	$Robberpfp.visible = true
+	$"S12-text4".visible = true
+	$"S12-text4/AnimationPlayer".play("S12-text4")
--- Scenes/transition.gd
+++ Scenes/transition.gd
@@ -5,14 +5,8 @@ extends CanvasLayer
 
 func _ready() -> void:
 	color_rect.visible = false
-	animation_player.animation_finished.connect(_on_animation_finished)
-
+	
 func _transition():
 	color_rect.visible = true
-	animation_player.play("fade_to_black")
-
-func _on_animation_finished(anim_name):
-	if anim_name == "fade_to_black":
-		animation_player.play("fade_to_normal")
-	elif anim_name == "fade_to_normal":
-		color_rect.visible = false
+	animation_player.play("cutscene")
+	
