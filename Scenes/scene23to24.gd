95bc9fb44e4e244a29d8e05683685508676da932
vishyyyyyyyyy <217375979+vishyyyyyyyyy@users.noreply.github.com>
2025-08-02 11:08:39 -0500

    scene23

---
+++ Scenes/scene23to24.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+var entered = false
+
+func _on_body_entered(body: CharacterBody2D) -> void:
+	if not entered:  # Prevent calling multiple times
+		entered = true
+		get_tree().change_scene_to_file("res://Scenes/scene24.tscn")
+
+func _on_body_exited(body: CharacterBody2D) -> void:
+	entered = false
