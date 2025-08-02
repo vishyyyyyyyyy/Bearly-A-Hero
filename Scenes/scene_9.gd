23879715a0cdde968e3d3b54f89417ff58ff5989
Aadityan Anbumani <aadityan4906@gmail.com>
2025-08-02 11:24:46 -0500

    scene 6 and 9 fixed

---
+++ Scenes/scene_9.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+
+func _ready():
+	$Timer1.start()
+	$Textplaceholder.visible = true
+	$Text1.visible = true
+	$Pfpplaceholder.visible = true
+	$Robberpfp.visible = true
+
+
+func _on_timer_1_timeout():
+	$Text1.visible = false
+	$Text2.visible = true
+	$Robberpfp.visible = true
+	$Pfpplaceholder.visible = true
+	$Textplaceholder.visible = true
+	$Timer2.start()  # Start next timer only after Timer1
+	
+
+
+func _on_timer_2_timeout():
+	$Text2.visible = false
+	$Pfpplaceholder.visible = false
+	$Robberpfp.visible = false
+	$Textplaceholder.visible = false
+	$Text3.visible = true
+	$CharacterBody2D.can_move = true
