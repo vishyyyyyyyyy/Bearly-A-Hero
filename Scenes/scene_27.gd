ccb60d457d5c9a1052f535711af4995b49a186be
vishyyyyyyyyy <217375979+vishyyyyyyyyy@users.noreply.github.com>
2025-08-02 13:21:09 -0500

    scene27

--- Scenes/scene_27.gd
+++ Scenes/scene_27.gd
@@ -12,22 +12,58 @@ func _process(delta: float) -> void:
 	pass
 
 func _animatedText():
-	$Node/AnimationPlayer/Label3.visible = true
-	$Node/AnimationPlayer.play("new_animation_3")
+	#txt1
+	$Node/AnimationPlayer/Label.visible = true
+	$Node/AnimationPlayer.play("1")
 	await $Node/AnimationPlayer.animation_finished
-	$Node/AnimationPlayer/Label3.visible = false
+	$Node/AnimationPlayer/Label.visible = false
 	$Bearpfp.visible = false
 	$Pfpplaceholder.visible = false
 	
-	
-	
-	
-	$Node/AnimationPlayer/Label.visible = true
-	$Node/AnimationPlayer.play("new_animation")
-	await $Node/AnimationPlayer.animation_finished
-	$Node/AnimationPlayer/Label.visible = false
-	
+	#txt2
+	$Pfpplaceholder2.visible = true
+	$Bunnynocry.visible = true
 	$Node/AnimationPlayer/Label2.visible = true
-	$Node/AnimationPlayer.play("new_animation_2")
+	$Node/AnimationPlayer.play("2")
 	await $Node/AnimationPlayer.animation_finished
 	$Node/AnimationPlayer/Label2.visible = false
+	$Bunnynocry.visible = false
+
+#txt3
+	$Birdnocry.visible = true
+	$Node/AnimationPlayer/Label3.visible = true
+	$Node/AnimationPlayer.play("3")
+	await $Node/AnimationPlayer.animation_finished
+	$Node/AnimationPlayer/Label3.visible = false
+	$Birdnocry.visible = false
+	
+	#txt4
+	$Deernocry.visible = true
+	$Node/AnimationPlayer/Label4.visible = true
+	$Node/AnimationPlayer.play("4")
+	await $Node/AnimationPlayer.animation_finished
+	$Node/AnimationPlayer/Label4.visible = false
+	$Deernocry.visible = false
+	
+	$Pfpplaceholder2.visible = false
+	$Textplaceholder.visible = false
+	$Node/AnimationPlayer/Label5.visible = true
+	$Node/AnimationPlayer.play("5")
+	await $Node/AnimationPlayer.animation_finished
+	$Node/AnimationPlayer/Label5.visible = false
+	
+	$Node/AnimationPlayer/Label6.visible = true
+	$Node/AnimationPlayer.play("6")
+	await $Node/AnimationPlayer.animation_finished
+	$Node/AnimationPlayer/Label6.visible = false
+	
+	
+	
+	$Node/AnimationPlayer/Label7.visible = true
+	$Node/AnimationPlayer.play("7")
+	await $Node/AnimationPlayer.animation_finished
+	
+	
+	
+	
+	
