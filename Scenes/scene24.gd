17dd67f990a643f4937691cc7bfeb588a02d5c1e
Aadityan Anbumani <aadityan4906@gmail.com>
2025-08-02 14:51:40 -0500

    Added camera pan for guard dog section

--- Scenes/scene24.gd
+++ Scenes/scene24.gd
@@ -1,10 +1,18 @@
 extends Node2D
 
 @onready var kill_timer: Timer = $guardDog5/Area2D/killTimer
+@onready var pan_camera: Camera2D = $Sprite2D2/panCamera
+@onready var camera_2d: Camera2D = $CharacterBody2D/Camera2D
+
+
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	pan_camera.make_current()
+	$Sprite2D2/AnimationPlayer.play("new_animation")
+	await $Sprite2D2/AnimationPlayer.animation_finished
+	camera_2d.make_current()
 	$CharacterBody2D.can_move = true # Replace with function body.
 
 
